f3ac3d90da8e25d98a58408928641cca12978037
lzyats <lzyats@qq.com>
2025-09-01 21:43:05 +0800

    2.0.0

--- src/main/java/com/platform/modules/quartz/dao/QuartzLogDao.java
+++ src/main/java/com/platform/modules/quartz/dao/QuartzLogDao.java
@@ -1,7 +1,10 @@
 package com.platform.modules.quartz.dao;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.platform.common.web.dao.BaseDao;
 import com.platform.modules.quartz.domain.QuartzLog;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +19,10 @@ public interface QuartzLogDao extends BaseDao<QuartzLog> {
      * 获取quartz调度器日志的计划任务
      */
     List<QuartzLog> queryList(QuartzLog quartzLog);
+
+    /**
+     * 删除日志
+     */
+    int dellogs(@Param(Constants.WRAPPER) QueryWrapper<QuartzLog> ew);
 
 }
